7d52816ab19508a14da77f4cf2e81a1e8cb3e3d3
Ben McGeachy <ben.mcgeachy@capgemini.com>
2014-01-16 12:23:00 +0000

    Working media query example

--- src/app/templates.js
+++ src/app/templates.js
@@ -3,5 +3,31 @@ angular.module('templates', ['app/app.view.html']);
 angular.module('app/app.view.html', []).run(['$templateCache', function($templateCache) {
 	'use strict';
 	$templateCache.put('app/app.view.html',
-		'<h1>{{message}}</h1>');
+		'<div class="small-10 medium-10 large-10 small-centered columns">\n' +
+		'	<h1>Media Queries</h1>\n' +
+		'	<div class="row">\n' +
+		'		<div class="small-4 medium-6 large-8 columns large-box"></div>\n' +
+		'		<h4 class="small-8 medium-6 large-4 columns">This box will be red if your viewport is larger than 1280px</h4>\n' +
+		'	</div>\n' +
+		'\n' +
+		'	<div class="row">\n' +
+		'		<div class="small-4 medium-6 large-8 columns medium-box"></div>\n' +
+		'		<h4 class="small-8 medium-6 large-4 columns">This box will be yellow if your viewport is between 768px and 1280px</h4>\n' +
+		'	</div>\n' +
+		'\n' +
+		'	<div class="row">\n' +
+		'		<div class="small-4 medium-6 large-8 columns small-box"></div>\n' +
+		'		<h4 class="small-8 medium-6 large-4 columns">This box will be green if your viewport is smaller than 768px</h4>\n' +
+		'	</div>\n' +
+		'\n' +
+		'	<div class="browser-dimenstions">\n' +
+		'		<div class="small-12 medium-6 large-6 columns">\n' +
+		'			<h3>Current Browser Width:</h3>&nbsp;<h3 id="jsWidth"></h3><h3>px</h3>\n' +
+		'		</div>\n' +
+		'		<div class="small-12 medium-6 large-6 columns">\n' +
+		'			<h3>Current Browser Height:</h3>&nbsp;<h3 id="jsHeight"></h3><h3>px</h3>\n' +
+		'		</div>\n' +
+		'	</div>\n' +
+		'\n' +
+		'</div>');
 }]);
