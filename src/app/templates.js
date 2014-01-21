3c3186ae473129911494b35e3dfae66201e47801
Ben McGeachy <ben.mcgeachy@capgemini.com>
2014-01-21 10:26:59 +0000

    Adding media query to inside of blocks

--- src/app/templates.js
+++ src/app/templates.js
@@ -7,23 +7,23 @@ angular.module('app/app.view.html', []).run(['$templateCache', function($templat
 		'	<h1>Media Queries</h1>\n' +
 		'\n' +
 		'	<!-- Top rectangle to be coloured red if the screen is large -->\n' +
-		'	<div class="row">\n' +
-		'		<div class="small-4 medium-6 large-8 columns large-box"></div>\n' +
-		'		<h4 class="small-8 medium-6 large-4 columns">This box will be red if your viewport is larger than 1280px</h4>\n' +
+		'	<div class="row media-block">\n' +
+		'		<div class="small-12 medium-6 large-8 columns large-box"><p>@media only screen and (min-width: 1281px)</p></div>\n' +
+		'		<h4 class="small-12 medium-6 large-4 columns">This box will be red if your viewport is larger than 1280px</h4>\n' +
 		'	</div>\n' +
 		'	<!-- End Rectangle -->\n' +
 		'\n' +
 		'	<!-- Middle rectangle to be coloured yellow if the screen is medium -->\n' +
-		'	<div class="row">\n' +
-		'		<div class="small-4 medium-6 large-8 columns medium-box"></div>\n' +
-		'		<h4 class="small-8 medium-6 large-4 columns">This box will be yellow if your viewport is between 768px and 1280px</h4>\n' +
+		'	<div class="row media-block">\n' +
+		'		<div class="small-12 medium-6 large-8 columns medium-box"><p>@media only screen and (min-width: 768px) and (max-width: 1280px)</p></div>\n' +
+		'		<h4 class="small-12 medium-6 large-4 columns">This box will be yellow if your viewport is between 768px and 1280px</h4>\n' +
 		'	</div>\n' +
 		'	<!-- End Rectangle -->\n' +
 		'\n' +
 		'	<!-- Bottom rectangle to be coloured green if the screen is small -->\n' +
-		'	<div class="row">\n' +
-		'		<div class="small-4 medium-6 large-8 columns small-box"></div>\n' +
-		'		<h4 class="small-8 medium-6 large-4 columns">This box will be green if your viewport is smaller than 768px</h4>\n' +
+		'	<div class="row media-block">\n' +
+		'		<div class="small-12 medium-6 large-8 columns small-box"><p>@media only screen and (max-width: 768px)</p></div>\n' +
+		'		<h4 class="small-12 medium-6 large-4 columns">This box will be green if your viewport is smaller than 768px</h4>\n' +
 		'	</div>\n' +
 		'	<!-- End Rectangle -->\n' +
 		'\n' +
