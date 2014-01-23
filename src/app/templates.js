8ac1d74e96b205efe63abbfc7e5961c0190f7c53
Ben McGeachy <ben.mcgeachy@capgemini.com>
2014-01-23 11:33:52 +0000

    Inital Commit

--- src/app/templates.js
+++ src/app/templates.js
@@ -3,5 +3,5 @@ angular.module('templates', ['app/app.view.html']);
 angular.module('app/app.view.html', []).run(['$templateCache', function($templateCache) {
 	'use strict';
 	$templateCache.put('app/app.view.html',
-		'<h1>{{message}}</h1>');
+		'<h1>Flex Box</h1>');
 }]);
