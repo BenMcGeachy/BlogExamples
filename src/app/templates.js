77126d70647ef5a7cecc5a18ad1731fb63a93be7
Ben McGeachy <ben.mcgeachy@capgemini.com>
2014-01-24 10:34:03 +0000

    Adding extra pages and organising file structure

--- src/app/templates.js
+++ src/app/templates.js
@@ -4,32 +4,8 @@ angular.module('app/app.view.html', []).run(['$templateCache', function($templat
 	'use strict';
 	$templateCache.put('app/app.view.html',
 		'<div class="small-10 medium-10 large-10 small-centered columns flexExamples">\n' +
-		'  <h1>Flexible Box Layout</h1>\n' +
-		'\n' +
-		'	<div class="row">\n' +
-		'		<div class="small-12 columns">\n' +
-		'			<div class="red"></div>\n' +
-		'			<div class="yellow"></div>\n' +
-		'			<div class="green"></div>\n' +
-		'		</div>\n' +
-		'	</div>\n' +
-		'	<div class="row">\n' +
-		'		<h4>This is how 3 blocks, with a defined width would look before any flex properties are defined.</h4>\n' +
-		'	</div>\n' +
-		'\n' +
-		'	<hr>\n' +
-		'\n' +
-		'	<div class="row">\n' +
-		'		<div class="small-12 columns flex-container">\n' +
-		'			<div class="red"></div>\n' +
-		'			<div class="yellow"></div>\n' +
-		'			<div class="green"></div>\n' +
-		'		</div>\n' +
-		'	</div>\n' +
-		'	<div class="row">\n' +
-		'		<h4>However, when "justify-content" is applied with the parameter "space-between", all blocks appear on the same line with even spacing between each item. In addition to this, their width\'s dynamically change depending on the screen size.</h4>\n' +
-		'	</div>\n' +
-		'\n' +
+		'	<h1><a href="exampleOne.html">Example One</a></h1>\n' +
+		'	<h1><a href="exampleTwo.html">Example Two</a></h1>\n' +
 		'</div>');
 }]);
 
